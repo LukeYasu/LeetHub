2610bea92815065d237fc314c6193585ab37ce45
Lucas Yasumura <lucasyasumura@gmail.com>
2024-12-06 18:24:38 -0800

    Time: 79 ms (15.73%), Space: 52.8 MB (37.47%) - LeetHub

--- 0001-two-sum/0001-two-sum.ts
+++ 0001-two-sum/0001-two-sum.ts
@@ -1,12 +1,10 @@
 function twoSum(nums: number[], target: number): number[] {
     for(let i = 0; i < nums.length; i++){
-        // if(nums[i] <= target){
-            if(nums.includes(target - nums[i])){
-                const ans1 = nums.findIndex((num)=> num === target - nums[i])
-                if(i !== ans1){
-                return [i, ans1]
-                }
+        if(nums.includes(target - nums[i])){
+            const ans1 = nums.findIndex((num)=> num === target - nums[i])
+            if(i !== ans1){
+            return [i, ans1]
             }
         }
-    // }
+    }
 };
