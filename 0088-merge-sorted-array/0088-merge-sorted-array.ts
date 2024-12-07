dbfc7d9aa02d06645194c5af83505fc78526456d
Lucas Yasumura <lucasyasumura@gmail.com>
2024-12-06 21:15:36 -0800

    Time: 0 ms (100.00%), Space: 52.1 MB (10.75%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.ts
+++ 0088-merge-sorted-array/0088-merge-sorted-array.ts
@@ -5,14 +5,10 @@ function merge(nums1: number[], m: number, nums2: number[], n: number): void {
     let mergedArr = [];
     let zeroCount = [];
     for(let i = 0; i < m; i++){
-
-            mergedArr.push(nums1[i])
-        
+        mergedArr.push(nums1[i])
     }
     for(let i = 0; i < n; i++){
-
-            mergedArr.push(nums2[i])
-        
+        mergedArr.push(nums2[i])
     }
     mergedArr.sort((a, b) => a - b)
     for(let i = 0; i < mergedArr.length; i++){
