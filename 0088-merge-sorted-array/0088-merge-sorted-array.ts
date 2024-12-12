f6fb3e6aafce8e7bc926b1c506908858591e4b15
Lucas Yasumura <lucasyasumura@gmail.com>
2024-12-12 14:03:09 -0800

    Time: 6 ms (7.43%), Space: 53.5 MB (5.07%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.ts
+++ 0088-merge-sorted-array/0088-merge-sorted-array.ts
@@ -2,16 +2,9 @@
  Do not return anything, modify nums1 in-place instead.
  */
 function merge(nums1: number[], m: number, nums2: number[], n: number): void {
-    let mergedArr = [];
-    let zeroCount = [];
-    for(let i = 0; i < m; i++){
-        mergedArr.push(nums1[i])
-    }
-    for(let i = 0; i < n; i++){
-        mergedArr.push(nums2[i])
-    }
-    mergedArr.sort((a, b) => a - b)
-    for(let i = 0; i < mergedArr.length; i++){
-        nums1[i] = mergedArr[i]
-    }
+    nums1.splice(m, nums1.length - m);
+    nums2.splice(n, nums2.length - n);
+    nums1.push(...nums2);
+    nums1.sort((a,b)=>a-b);
+console.log(nums1)
 }
