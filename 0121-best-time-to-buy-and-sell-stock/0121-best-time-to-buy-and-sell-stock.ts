0935208f1b48a84609b42c8c85d23bec9ffc6a61
Lucas Yasumura <lucasyasumura@gmail.com>
2024-12-17 17:30:57 -0800

    Time: 2 ms (76.10%), Space: 60 MB (46.28%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.ts
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.ts
@@ -1,7 +1,6 @@
 function maxProfit(prices: number[]): number {
     let priceDiff = 0
     let buyPrice = prices[0]
-    let sellPrice = 0
     for(let i = 1; i < prices.length; i++){
         if(buyPrice > prices[i]){
             buyPrice = prices[i]
