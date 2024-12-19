2e8a26660f1495883445a615b931e977865cf864
Lucas Yasumura <lucasyasumura@gmail.com>
2024-12-19 00:34:13 -0800

    Time: 7 ms (5.15%), Space: 52.3 MB (5.10%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.ts
@@ -0,0 +1,11 @@
+function climbStairs(n: number): number {
+    if(n === 1) return 1;
+    if(n === 2) return 2;
+    let arr = [0,1,2];
+    for(let i = 3; i <= n; i++){
+        const num = arr[i - 1] + arr[i - 2];
+        arr.push(num);
+    };
+    console.log(arr)
+    return arr[n];
+};
