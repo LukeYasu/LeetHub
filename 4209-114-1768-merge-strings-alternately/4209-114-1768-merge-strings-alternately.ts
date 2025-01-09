c7760aa1c4ecacd749e65bbf386b75ed6aa2a68a
Lucas Yasumura <lucasyasumura@gmail.com>
2025-01-09 14:40:16 -0800

    Time: 72 ms, Memory: 52.2 MB - LeetHub

---
+++ 4209-114-1768-merge-strings-alternately/4209-114-1768-merge-strings-alternately.ts
@@ -0,0 +1,12 @@
+function mergeAlternately(word1: string, word2: string): string {
+    let mergedString = ''
+    const longerWord = word1.length > word2.length ? word1 : word2
+    for(let i = 0; i < longerWord.length; i++){
+      mergedString += word1[i] ?? ''
+      mergedString += word2[i] ?? ''
+    }
+    console.log(mergedString)
+    return mergedString
+};
+
+mergeAlternately('aaaa', 'bbbb')
