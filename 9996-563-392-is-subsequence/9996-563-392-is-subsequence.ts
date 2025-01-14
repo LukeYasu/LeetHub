65c7b05cec90062875de52bd2de6098470fa5ce1
Lucas Yasumura <lucasyasumura@gmail.com>
2025-01-13 18:14:04 -0800

    Time: 2 ms, Memory: 51.2 MB - LeetHub

--- 9996-563-392-is-subsequence/9996-563-392-is-subsequence.ts
+++ 9996-563-392-is-subsequence/9996-563-392-is-subsequence.ts
@@ -1,11 +1,9 @@
 function isSubsequence(s: string, t: string): boolean {
-    let x = 0
-    if(s === '') return true
+    let x = 0;
+    if(s === '') return true;
     for(let i = 0; i < t.length; i++){
-        if(s[x] === t[i]){
-            x++
-        }
-        if(x === s.length) return true
-    }
-    return false
+        if(s[x] === t[i]) x++;
+        if(x === s.length) return true;
+    };
+    return false;
 };
