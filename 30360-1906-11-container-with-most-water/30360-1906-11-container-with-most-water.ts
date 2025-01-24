dd9877cac647ba7f74a1938fa7e4cead7280559e
Lucas Yasumura <lucasyasumura@gmail.com>
2025-01-23 19:02:53 -0800

    Time: 2 ms, Memory: 59.3 MB - LeetHub

--- 30360-1906-11-container-with-most-water/30360-1906-11-container-with-most-water.ts
+++ 30360-1906-11-container-with-most-water/30360-1906-11-container-with-most-water.ts
@@ -1,11 +1,10 @@
 function maxArea(height: number[]): number {
     let start = 0;
     let right = height.length - 1;
-    let shorterHeight = 0;
     let maxVolume = 0;
     while(start !== right){
-        shorterHeight = height[start] < height[right] ? height[start] : height[right];
-        maxVolume = shorterHeight * (right - start) > maxVolume ? shorterHeight * (right - start) : maxVolume;
+        let shorterHeight = Math.min(height[start], height[right]);
+        maxVolume = Math.max(maxVolume, shorterHeight * (right - start));
         if(height[start] < height[right]){
             start++;
         } else {
