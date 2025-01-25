2d3de37e573a6e0bdd8232178818c47c3ee70673
Lucas Yasumura <lucasyasumura@gmail.com>
2025-01-24 16:17:31 -0800

    Time: 55 ms, Memory: 51.3 MB - LeetHub

--- 2185-1766-1108-defanging-an-ip-address/2185-1766-1108-defanging-an-ip-address.ts
+++ 2185-1766-1108-defanging-an-ip-address/2185-1766-1108-defanging-an-ip-address.ts
@@ -1,11 +1,7 @@
 function defangIPaddr(address: string): string {
     let addy = '';
     for(let i = 0; i < address.length; i++){
-        if(address[i] === '.'){
-            addy += '[.]';
-        } else {
-            addy += address[i];
-        };
+        addy += address[i] === '.' ? '[.]' : address[i];
     };
     return addy;
 };
