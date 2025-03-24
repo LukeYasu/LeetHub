55a8334fa249bcb36f241b607a71759692e762b3
Lucas Yasumura <lucasyasumura@gmail.com>
2025-03-23 17:58:59 -0700

    Time: 3 ms (39.71%), Space: 59.6 MB (15.81%) - LeetHub

--- 0941-sort-array-by-parity/0941-sort-array-by-parity.ts
+++ 0941-sort-array-by-parity/0941-sort-array-by-parity.ts
@@ -2,10 +2,13 @@ function sortArrayByParity(nums: number[]): number[] {
     let result = [];
     for(let i = 0; i < nums.length; i++){
         if(nums[i] % 2 === 0){
-            result.unshift(nums[i])
-        } else {
+            result.push(nums[i]);
+        }
+    };
+    for(let i = 0; i < nums.length; i++){
+        if(nums[i] % 2 !== 0){
             result.push(nums[i])
         }
     }
-    return result
+    return result;
 };
