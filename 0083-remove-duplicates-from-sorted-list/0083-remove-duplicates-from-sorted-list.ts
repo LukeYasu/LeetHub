677b2855d58f6ec1f3e3b1ce51aee4ef4aa703cb
Lucas Yasumura <lucasyasumura@gmail.com>
2025-03-31 22:01:29 -0700

    Time: 0 ms (100%), Space: 58.3 MB (48.9%) - LeetHub

---
+++ 0083-remove-duplicates-from-sorted-list/0083-remove-duplicates-from-sorted-list.ts
@@ -0,0 +1,24 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+function deleteDuplicates(head: ListNode | null): ListNode | null {
+    let current = head
+    let prev = null
+    while(head !== null && head.next !== null){
+        if(head.val === head.next.val){
+            head.next = head.next.next
+        } else {
+            head = head.next
+        }
+    }
+    return current
+};
