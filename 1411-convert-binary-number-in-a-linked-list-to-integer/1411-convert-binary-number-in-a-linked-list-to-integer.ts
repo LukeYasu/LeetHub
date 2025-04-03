4ec0dff720ced000c622e3174d8fa20d690f4c71
Lucas Yasumura <lucasyasumura@gmail.com>
2025-04-02 23:31:01 -0700

    Time: 0 ms (100%), Space: 56 MB (22.5%) - LeetHub

---
+++ 1411-convert-binary-number-in-a-linked-list-to-integer/1411-convert-binary-number-in-a-linked-list-to-integer.ts
@@ -0,0 +1,20 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+function getDecimalValue(head: ListNode | null): number {
+    let total = 0;
+    while(head !== null){
+        total = total * 2 + head.val;
+        head = head.next; 
+    };
+    return total;
+};
